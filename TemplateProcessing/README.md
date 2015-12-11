9699e1f269bd3a0d9203399ec897579018e520c5
matt mcgraw <matthew.mcgraw@gmail.com>
2015-12-11 14:06:34 -0500

    initial commit with template processing

---
+++ TemplateProcessing/README.md
@@ -0,0 +1,20 @@
+## Python vs VBA/BASIC for Corporate Budget Processing
+
+Within the world of financial services and to a (slightly) lesser extent corporate finance the toolkit of Excel + VBA continues to reign supreme.  Colleagues of mine still use VBA for data processing and computation, and I wanted to compare the usage of Python and VBA.
+
+It's worth noting a few things.  First, and perhaps foremost, I am not using VBA but rather BASIC, which is the scripting language for OpenOffice/LibreOffice.  It's documentation isn't as complete as one might hope, but there are some helpful guides around the web.  Additionally, my skill in Python likely exceeds my skill in BASIC, so there are likely inefficiencies in my BASIC code for which I apologize.
+
+I'll be going through a series of these 
+
+### Running Python Budget Processor
+```
+python3 budget_processor.py
+```
+No requirements, native Python.
+
+### Running BASIC Budget Processor
+```
+Within your LibreOffice Document
+Tools -> Macros -> Organize Macros -> LibreOffice Basic... -> New...
+Paste budget_processor.BASIC
+```
---
+++ TemplateProcessing/budget_processor.py
@@ -0,0 +1,41 @@
+import csv
+import os
+
+denormalized_data = [
+        ['cost_center', 'account', 'month', 'value'],
+    ]
+
+cc_prefix = "CC"
+months = [  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+files = []
+
+for newfile in os.listdir(os.getcwd()):
+    extns = newfile.split(".")
+    if len(extns) == 2 and extns[-1] == "csv" and extns[0].startswith("CC"):
+        files.append(newfile)
+        print (newfile)
+
+for afile in files:
+    next_cc_name = afile.split('_')[0]
+    cost_center = next_cc_name[next_cc_name.find(cc_prefix)+len(cc_prefix):]
+    with open(afile, newline='') as csvfile:
+        budgetreader = csv.reader(csvfile, delimiter='.', quotechar='"')
+        for row in budgetreader:
+            rowdata = (','.join(row))
+            row_data_list = rowdata.split(',')
+            for col in range(1, 13):
+                try:
+                    int(row_data_list[col])
+                except:
+                    continue
+                new_csv_row = [ cost_center,
+                                row_data_list[0],
+                                months[col-1],
+                                row_data_list[col]]
+                denormalized_data.append(new_csv_row)
+
+with open('output.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(denormalized_data)
